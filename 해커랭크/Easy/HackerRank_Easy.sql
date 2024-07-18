b6cf81848320ed19b272b378d3af92892ec7b5e3
seohyeonbin38 <seohyunbin2@gmail.com>
2024-07-18 22:33:53 +0900

    Update HackerRank_Easy.sql

--- 해커랭크/Easy/HackerRank_Easy.sql
+++ 해커랭크/Easy/HackerRank_Easy.sql
@@ -62,3 +62,9 @@ where city like '%a'
         or city like '%i'
         or city like '%o'
         or city like '%u'
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+select distinct city
+from station
+where left(city, 1) in ('a', 'e', 'i', 'o', 'u')
+and right(city, 1) in ('a', 'e', 'i', 'o', 'u');
