49e192597cbd111c3c19d7d2dfa5a101fc984b72
seohyeonbin38 <seohyunbin2@gmail.com>
2024-07-22 20:20:02 +0900

    Update HackerRank_Easy.sql

--- 해커랭크/Easy/HackerRank_Easy.sql
+++ 해커랭크/Easy/HackerRank_Easy.sql
@@ -68,3 +68,8 @@ select distinct city
 from station
 where left(city, 1) in ('a', 'e', 'i', 'o', 'u')
 and right(city, 1) in ('a', 'e', 'i', 'o', 'u');
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+select distinct city
+from station
+where city regexp '^[^aeiou]'
