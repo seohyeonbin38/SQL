efbcfe328aa3a4467dce4f9a090a8f07731550f2
name <seohyunbin2@gmail.com>
2024-08-13 23:11:43 +0900

    Update HackerRank_Easy.sql

--- 해커랭크/Easy/HackerRank_Easy.sql
+++ 해커랭크/Easy/HackerRank_Easy.sql
@@ -167,3 +167,8 @@ from city
 select ceil(avg(salary) - avg(replace(salary, 0, '')))
 from employees
 
+-- We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
+select (salary * months) as earnings, count(*)
+from employee
+group by earnings
+order by earnings desc limit 1
